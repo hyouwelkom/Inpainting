1aa7f26eb3fd60835dfc473b11dfe4cc3800c3e4
Unknown <remy.aune@etu.unice.fr>
2017-09-05 10:19:09 +0200

    Color.java
    Add isequalto

--- src/td/topology/Color.java
+++ src/td/topology/Color.java
@@ -1,13 +1,9 @@
 package td.topology;
 
 public class Color {
-    private byte B;
-    private byte G;
-    private byte R;
-    // Use the BGR color format
+
     // Definition of the color in BGR format 3x8bits val=[B,G,R] B,G,R = 8bits (Blue,Green,Red)
     private byte[] val;
-
 
     public Color(byte B, byte G, byte R) {
         val[0] = B;
@@ -15,26 +11,35 @@ public class Color {
         val[2] = R;
     }
 
+    //Cast int entry into byte
     public Color(int B, int G, int R) {
         val[0] = ((byte) B);
         val[1] = ((byte) G);
         val[2] = ((byte) R);
     }
 
-
-
+    //Return difference between two color
     public static int dist (Color c1, Color c2) {
-        return 0;
+        int dist = 0;
+        dist = ((c1.val[0] - c2.val[0])*(c1.val[0] - c2.val[0]))+((c1.val[1] - c2.val[1])*(c1.val[1] - c2.val[1]))+((c1.val[2] - c2.val[2])*(c1.val[2] - c2.val[2]));
+        return dist;
     }
 
 
-
-
-
     // Noice the name all in lowercase..
-    public boolean isequalto(Color color) { return false;}
+    public boolean isequalto(Color color) {
+        if (this.val[0] == color.val[0] ) {
+            if (this.val[1] == color.val[1] ) {
+                if (this.val[2] == color.val[2] ) {
+                    return true;
+                }else return false;
+            }else return false;
+        }else return false;
+    }
 
-    public void set(Color color) {}
+    public void set(Color color) {
+
+    }
 
     @Override
     public String toString() { return ""; }
