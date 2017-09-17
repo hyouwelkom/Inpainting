0db73ede452ea6d48d898a2afb675137995da59d
Fuchsy <alexandre68.fuchs@hotmail.fr>
2017-09-17 15:16:55 +0200

    Classe BoundingBox, il reste les méthodes à faire

--- src/td/topology/BoundingBox.java
+++ src/td/topology/BoundingBox.java
@@ -7,9 +7,43 @@ public class BoundingBox {
     int nbEdgesHorizontal, nbEdgesVertical, nbEdges;
 
     public BoundingBox() {}
-    public BoundingBox(java.awt.image.BufferedImage image) {}
-    public BoundingBox(int[] bb_) {}
-    public BoundingBox(java.lang.String fileName) {}
+    public BoundingBox(BoundingBox boundingBox) {
+        this.width = boundingBox.width;
+        this.height = boundingBox.height;
+        this.size = boundingBox.size;
+        this.bb = boundingBox.bb;
+    }
 
-    public BoundingBox crop(Patch patch) { return this; }
+    public BoundingBox(java.awt.image.BufferedImage image) {
+        this.width = image.getWidth();
+        this.height = image.getHeight();
+        this.size = this.height*this.width;
+    }
+    public BoundingBox(int[] bb_) {
+        this.width = bb[2]-bb[0];
+        this.height = bb[3]-bb[1];
+        this.bb = bb_;
+        this.size= this.height*this.width;
+    }
+    public BoundingBox(java.lang.String fileName) {
+        try
+        {
+           BufferedImage img = ImageIO.read(new File(fileName));
+            this.width = img.getWidth();
+            this.height = img.getHeight();
+            this.size = this.width*this.height;
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    public BoundingBox crop(Patch patch) { //Il reste ça ç faire
+
+        return this;
+
+
+    }
 }
